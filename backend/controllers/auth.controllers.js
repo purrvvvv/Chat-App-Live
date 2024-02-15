c65b80320f5962c77964d7526192dd2a0a37b900
purrvvvv <purvshah10@gmail.com>
2024-02-16 03:32:51 +0530

    Initial commit

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const signup = (req,res) => {
+    console.log("SignupUser");
+}
+export const login = (req,res) => {
+    console.log("loginUser");
+}
+export const logout = (req,res) => {
+    console.log("logoutUser");
+}
