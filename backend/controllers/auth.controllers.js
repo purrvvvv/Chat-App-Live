b6649fad34deca158c6398ca3176b5c9b9e9a1b7
purrvvvv <purvshah10@gmail.com>
2024-02-17 20:43:27 +0530

    MongoDB connected

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,46 @@
-export const signup = (req,res) => {
-    console.log("SignupUser");
+import User from "../models/user.model.js";
+
+export const signup = async (req,res) => {
+   try {
+
+    const {fullName,username,password,confirmPassword,gender}= req.body;
+
+    if(password != confirmPassword){
+        return res.status(400).json({error:"Passwords do not match"})
+    }
+
+    const user=  await User.findOne({username});
+
+    if(user){
+
+        return res.status(400).json({error:"User already exist!"})
+    }
+    
+    const boyProfilePic= `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic= `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+        fullName,
+        username,
+        password,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+        _id:newUser._id,
+        fullName:newUser.fullName,
+        username:newUser.username,
+        profilePic:newUser.profilePic
+    })
+
+
+   } catch (error) {
+    console.log("error in signup controller",error.message);
+    res.status(500).json({error:"Internal Server error!"});
+   }
 }
 export const login = (req,res) => {
     console.log("loginUser");
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import ConnectToMongoDB from "./db/connectToMongoDB.js";
 
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000; // Using port 8080 as an example
+dotenv.config();
+
+app.use(express.json()); // to handle the incoming requests with JSON  (from req.body)
 
 app.get("/", (req, res) => {
   res.send("Hello World hyyy");
 });
-
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  ConnectToMongoDB(); 
+  console.log(`Server is running on port ${PORT}`)
+});
