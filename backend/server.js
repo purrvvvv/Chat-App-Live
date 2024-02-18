e2f7305ec51ad615772b3d84e1061dbcd3944fb1
purrvvvv <purvshah10@gmail.com>
2024-02-18 18:20:37 +0530

    send message done

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import ConnectToMongoDB from "./db/connectToMongoDB.js";
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000; // Using port 8080 as an example
 dotenv.config();
 
 app.use(express.json()); // to handle the incoming requests with JSON  (from req.body)
+
+app.use(cookieParser()); 
 
 app.get("/", (req, res) => {
   res.send("Hello World hyyy");
