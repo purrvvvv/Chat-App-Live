fa75cd25aaa687f9d4c50de8d6bb7c019603ea45
purrvvvv <purvshah10@gmail.com>
2024-02-29 02:05:19 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,7 +11,9 @@ import { app, server } from "./socket/socket.js";
 import ConnectToMongoDB from "./db/connectToMongoDB.js";
 
 
-const PORT = process.env.PORT || 5000; // Using port 8080 as an example
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve(); // to use __dirname with ES6 modules
+// Using port 8080 as an example
 dotenv.config();
 
 app.use(express.json()); // to handle the incoming requests with JSON  (from req.body)
@@ -24,6 +26,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   ConnectToMongoDB(); 
