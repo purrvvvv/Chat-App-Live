096b8d455c173e5f9092233e0ef9f03c10dcd32e
purrvvvv <purvshah10@gmail.com>
2024-02-29 02:23:17 +0530

    deployed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-live-zj4x.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
